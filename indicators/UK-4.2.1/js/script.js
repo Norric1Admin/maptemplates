ae225ec064b4d0312f9b0ecca6fb46f0aee68b58
Norric1Admin <37659239+Norric1Admin@users.noreply.github.com>
2018-10-09 10:09:23 +0100

    Update script.js

--- indicators/UK-4.2.1/js/script.js
+++ indicators/UK-4.2.1/js/script.js
@@ -1000,7 +1000,7 @@ if (Modernizr.webgl) {
 //Add
 				var xAxisTime = d3.axisBottom(x)
 					.tickSize(5)
-					.tickValues([dvc.timepoints[0], dvc.timepoints[10]]))
+					.tickValues([dvc.timepoints[0], dvc.timepoints[10]])
 					.tickFormat(legendformat);
 
 				var g = svgkey.append("g").attr("id","vert")
